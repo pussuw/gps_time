85e4054514c402eb52d517051a453584ea64224d
Ville <ville.juven@wirepas.com>
2015-04-23 15:17:12 +0300

    Changed directory structure
    Compiler output is now directed to build-folder.
    Sources are in a separate folder.

---
+++ sources/gps_reader_node/gps_reader.c
@@ -0,0 +1,8 @@
+/*
+ * gps_reader.c
+ *
+ *  Created on: 23.4.2015
+ *      Author: Ville
+ */
+
+
---
+++ sources/gps_reader_node/gps_reader.h
@@ -0,0 +1,13 @@
+/*
+ * gps_reader.h
+ *
+ *  Created on: 23.4.2015
+ *      Author: Ville
+ */
+
+#ifndef GPS_READER_H_
+#define GPS_READER_H_
+
+
+
+#endif /* GPS_READER_H_ */
