2715d664e990bfd2b2105d30798cb943297b059c
Ville <ville.juven@wirepas.com>
2015-04-24 15:58:25 +0300

    Very simple interval timer

--- sources/common/timer.c
+++ sources/common/timer.c
@@ -6,8 +6,13 @@
  */
 
 #include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
 #include "nrf.h"
 #include "timer.h"
+#include "hal.h"
+#include "interrupt.h"
+#include "myassert.h"
 
 /* Prescaler to achieve 1MHz frequency
  * f(timer) = HFCLK / 2^psc
@@ -16,14 +21,27 @@
  * 2^psc = 16 if clock = 16MHz -> psc = 4
  * 2^psc = 32 if clock = 32MHz -> psc = 5 */
 #if (__SYSTEM_CLOCK == 16000000)
-#define TIMER_PRESCALER         4
+#define TIMER_PRESCALER             4
 #else
-#define TIMER_PRESCALER         5
+#define TIMER_PRESCALER             5
 #endif
 
 /* Capture / Compare channels */
-#define TIMER_COUNT_CC          0
-#define TIMER_TIMEOUT_CC        1
+#define TIMER_COUNT_CC              0
+#define TIMER_TIMEOUT_CC            1
+#define TIMER_INTERVAL_CC           2
+
+/* Some margin for timeouts, so that waiting does not take forever (4300s) */
+#define MINIMUM_TIMEOUT_TIME        5
+
+/* Interval service */
+#define MINIMUM_INTERVAL_TIME       1000
+#define MAXIMUM_BIAS_TIME(time)     ((time) / 2)
+#define INTERVAL_TIMER_ENABLE       TIMER_INTENSET_COMPARE2_Msk
+#define INTERVAL_TIMER_DISABLE      TIMER_INTENCLR_COMPARE2_Msk
+static volatile uint32_t            m_interval_time;
+static volatile uint32_t            m_max_bias_time;
+static volatile timer_callback_f    m_interval_cb;
 
 void Timer_init(void)
 {
@@ -43,6 +61,10 @@ void Timer_init(void)
     NRF_TIMER0->PRESCALER = TIMER_PRESCALER;
     NRF_TIMER0->BITMODE = TIMER_BITMODE_BITMODE_32Bit;
     NRF_TIMER0->INTENCLR = 0xFFFFFFFF;
+    /* Enable NVIC channel for timer */
+    NVIC_ClearPendingIRQ(TIMER0_IRQn);
+    NVIC_SetPriority(TIMER0_IRQn, HAL_TIMER_INTERRUPT_PRIO);
+    NVIC_EnableIRQ(TIMER0_IRQn);
     /* Start timer */
     NRF_TIMER0->TASKS_START = 1;
 }
@@ -53,8 +75,6 @@ uint32_t Timer_getCount(void)
     return NRF_TIMER0->CC[TIMER_COUNT_CC];
 }
 
-/* Some margin for timeouts, so that waiting does not take forever (4300s) */
-#define MINIMUM_TIMEOUT_TIME    5
 void Timer_setTimeout(uint32_t time)
 {
     /* Lets make sure timeout is not  too close */
@@ -84,6 +104,54 @@ void Timer_waitTimeout(void)
     Timer_clearTimeout();
 }
 
+bool Timer_registerIntervalCounter(uint32_t interval,
+                                   timer_callback_f cb,
+                                   int32_t bias)
+{
+    bool ret = false;
+    uint32_t max_bias;
+    Interrupt_disableAll();
+    max_bias = MAXIMUM_BIAS_TIME(interval);
+    if((abs(bias) <= max_bias) &&
+       (interval >= MINIMUM_INTERVAL_TIME) &&
+       (cb != NULL))
+    {
+        m_interval_time = interval;
+        m_max_bias_time = max_bias;
+        m_interval_cb = cb;
+        interval = Timer_getCount() + interval + bias;
+        NRF_TIMER0->EVENTS_COMPARE[TIMER_INTERVAL_CC] = 0;
+        NRF_TIMER0->CC[TIMER_INTERVAL_CC] = interval;
+        NRF_TIMER0->INTENSET = INTERVAL_TIMER_ENABLE;
+        ret = true;
+    }
+    Interrupt_enableAll();
+    return ret;
+}
+
+int32_t Timer_timeToIntervalTick(void)
+{
+    uint32_t time;
+    Interrupt_disableAll();
+    time = Timer_getCount();
+    time = NRF_TIMER0->CC[TIMER_INTERVAL_CC] - time;
+    Interrupt_enableAll();
+    return (int32_t)time;
+}
+
+void Timer_resetIntervalCounter(int32_t bias)
+{
+    uint32_t time;
+    assert(m_interval_cb != NULL);
+    assert(abs(bias) <= m_max_bias_time);
+    Interrupt_disableAll();
+    time = Timer_getCount();
+    time += m_interval_time + bias;
+    NRF_TIMER0->EVENTS_COMPARE[TIMER_INTERVAL_CC] = 0;
+    NRF_TIMER0->CC[TIMER_INTERVAL_CC] = time;
+    Interrupt_enableAll();
+}
+
 void __attribute__((optimize("-O0"))) Timer_delayMicros(uint32_t us)
 {
     volatile uint32_t start = Timer_getCount();
@@ -93,4 +161,20 @@ void __attribute__((optimize("-O0"))) Timer_delayMicros(uint32_t us)
 void __attribute__((optimize("-O0"))) Timer_delayMillis(uint32_t ms)
 {
     while (ms-- > 0) Timer_delayMicros(999);
+}
+
+void __attribute__((__interrupt__)) TIMER0_IRQHandler(void)
+{
+    if(NRF_TIMER0->EVENTS_COMPARE[TIMER_INTERVAL_CC])
+    {
+        uint32_t time;
+        NRF_TIMER0->EVENTS_COMPARE[TIMER_INTERVAL_CC] = 0;
+        time = NRF_TIMER0->CC[TIMER_INTERVAL_CC];
+        time += m_interval_time;
+        NRF_TIMER0->CC[TIMER_INTERVAL_CC] = time;
+        if(m_interval_cb)
+        {
+            m_interval_cb();
+        }
+    }
 }
--- sources/common/timer.h
+++ sources/common/timer.h
@@ -11,12 +11,23 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+typedef void(*timer_callback_f)(void);
+
 void Timer_init(void);
 uint32_t Timer_getCount(void);
 void Timer_setTimeout(uint32_t time);
 bool Timer_getTimeout(void);
 void Timer_clearTimeout(void);
 void Timer_waitTimeout(void);
+/**
+ * Interval in us, callback is mandatory, bias calibrates first timeout */
+bool Timer_registerIntervalCounter(uint32_t interval,
+                                   timer_callback_f cb,
+                                   int32_t bias);
+/** Get bias from timer (time to next tick in microseconds) */
+int32_t Timer_timeToIntervalTick(void);
+/** Use bias to move first interval back / forth (calibrate system time) */
+void Timer_resetIntervalCounter(int32_t bias);
 void Timer_delayMicros(uint32_t us);
 void Timer_delayMillis(uint32_t ms);
 
